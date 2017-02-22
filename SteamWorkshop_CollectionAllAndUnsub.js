68560a1c109c8fa19d9cab4f658326de704954e9
GABRIEL-PC\Gabriel <gabrielrf96@gmail.com>
2017-02-22 20:55:55 +0100

    Initial commit. Create basic script and functionality
    v1.0 - The script will add all subscribed Workshop items to a collection
    and then unsubscribe each of them. Steam username and appid of the
    game (whose subscribed items will be processed) are configurable inside
    the script.
    All items tagged as "mod" will be sent to the mods collection, and all other
    ones will be sent to the other collection.
    Error control is still pending.

---
+++ SteamWorkshop_CollectionAllAndUnsub.js
@@ -0,0 +1,63 @@
+﻿
+/*
+    iMacros JavaScript macro for Steam collection organization
+    SteamWorkshop_CollectionAllAndUnsub v1.0
+*/
+
+var appId = 255710;         // Cities: Skylines hard-coded appId
+var userName = "eichiro";
+var modCollectionId = 852323535;
+var collectionId = 869286512;
+var iterationCount = 3;
+
+var initTime;
+var finishTime;
+
+var macroTextInit =
+    "TAB T=1" + "\n" +
+    "URL GOTO=http://steamcommunity.com/id/" + userName + "/myworkshopfiles/?appid=" + appId + "&browsefilter=mysubscriptions";
+var macroText2 =
+    "TAG POS=2 TYPE=SPAN ATTR=TXT:Añadir<SP>a<SP>colección" + "\n" +
+    "TAG POS=1 TYPE=INPUT:CHECKBOX ATTR=ID:";
+var macroText3 =
+    " CONTENT=YES" + "\n" +
+    "TAG POS=1 TYPE=SPAN ATTR=TXT:Aceptar" + "\n" +
+    "TAG POS=1 TYPE=DIV ATTR=TXT:Anular<SP>suscripción" + "\n" +
+    "WAIT SECONDS=1" + "\n" +
+    "BACK" + "\n" +
+    "REFRESH";
+
+function isMod() {
+    var items = window.content.document.getElementsByClassName("rightDetailsBlock");
+
+    for (var i=0 ; i<items.length ; i++) {
+        var item = items.item(i);
+        var tagLink = item.getElementsByTagName("a")[0];
+        if (tagLink != null && tagLink.innerHTML.toLowerCase().trim() === "mod")
+            return true;
+    }
+
+    return false;
+}
+
+function run() {
+    iimPlayCode("TAG POS=1 TYPE=DIV ATTR=CLASS:workshopItemTitle");
+    var targetCollectionId = isMod() ? modCollectionId:collectionId;
+    iimPlayCode(macroText2 + targetCollectionId + macroText3);
+}
+
+function calcTimeTaken(startedAt, finishedAt) {
+    var takenSeconds = Math.round( (finishedAt - startedAt) / 1000 );
+    return ( takenSeconds >= 60 ? (Math.round(takenSeconds/60) + " minutes"):(takenSeconds + " seconds") );
+}
+
+
+iimPlayCode(macroTextInit);
+initTime = new Date().getTime();
+for (var i=0 ; i<iterationCount ; i++) {
+    iimDisplay("Only [" + (iterationCount - i) + "] addons left.");
+    run();
+}
+finishTime = new Date().getTime();
+var timeTaken = calcTimeTaken(initTime, finishTime);
+iimDisplay("Done! Processed " + iterationCount + " addons on this run.\nIt took about " + timeTaken + " to get it done.");
